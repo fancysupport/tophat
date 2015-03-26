05e6535ea5142159267a484709b84566ac131e16
dstrek <batman@dstrek.com>
2015-03-27 02:26:58 +1100

    no need for this to be separate

--- client.go
+++ client.go
@@ -99,3 +99,28 @@ func (c *Client) Graph(mgr MetricGraphRequest) (*MetricGraph, error) {
 	// pass graph request
 	return m.Graph(conn, mgr)
 }
+
+func NewClient(pool *redis.Pool) (*Client, error) {
+
+	client := &Client{
+		pool:    pool,
+		steps:   map[string]*Timestep{},
+		metrics: map[string]*Metric{},
+	}
+
+	// preload default steps
+	if err := client.AddTimestep(TimestepHour); err != nil {
+		return nil, err
+	}
+	if err := client.AddTimestep(TimestepDay); err != nil {
+		return nil, err
+	}
+	if err := client.AddTimestep(TimestepMonth); err != nil {
+		return nil, err
+	}
+	if err := client.AddTimestep(TimestepYear); err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
